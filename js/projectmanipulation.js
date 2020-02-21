ec8a0202644121831f9bcc65c3fca3fab178026d
B.van.der.Wolf <benjamin.van.der.wolf@hva.nl>
2020-02-21 21:04:31 +0100

    Added a start on changing projects to show

--- js/projectmanipulation.js
+++ js/projectmanipulation.js
@@ -3,31 +3,114 @@ async function LoadDefaultInfo() {
   const response = await fetch("/projectinfo.json");
   const json = await response.json();
 
-  //get default entry or first
-  const dft = json["mathrecycler"];
+  //first entry is the default entry for the page
+  const keys = Object.keys(json);
+  const dft = json[keys[0]];
 
-  //get child elements
-  const img = document.getElementById("projectinfo-img");
-  const title = document.getElementById("projectinfo-title");
-  const description = document.getElementById("projectinfo-description");
-  const subtitle = document.getElementById("projectinfo-subtitle");
-  const detailslist = document.getElementById("projectinfo-detailslist");
+  //set all project info related elements
+  attachEventListeners();
+  setImageSources(getImageSources(json, keys));
+  setTitle(dft.title);
+  setDescription(dft.description);
+  setSubTitle(dft.subtitle);
+  setDetailsList(dft.detailslist);
+}
 
-  //assign values from json to elements
-  img.src = dft.imgSrc;
-  title.innerText = dft.title;
-  description.innerText = dft.description;
-  subtitle.innerText = dft.subtitle;
+async function onChangeInfo(element) {
+  //load json file
+  const response = await fetch("/projectinfo.json");
+  const json = await response.json();
 
-  const ls = dft["detailslist"];
-  for (let i = 0; i < ls.length; i++) {
-    let li = document.createElement("li");
-    li.appendChild(document.createTextNode(ls[i]));
-    detailslist.appendChild(li);
+  //get main img element
+  const mainImg = document.getElementById("projectinfo-img");
+
+  //get their current data value
+  const maindata = mainImg.dataset.project;
+  const otherdata = element.dataset.project;
+}
+
+function attachEventListeners() {
+  const otherImgs = document.getElementsByClassName("projectinfo-other");
+  for (const img of otherImgs) {
+    img.addEventListener("click", () => {
+      onChangeInfo(img);
+    });
   }
 }
 
-//Important! do checks on json values
-//make const for default value
-//split code more into functions
-//add more projects
+function getImageSources(json, keys) {
+  let srcArray = [];
+
+  if (!json || !keys) return srcArray;
+
+  for (let i = 0; i < keys.length; i++) {
+    const src = json[keys[i]].imgSrc;
+    if (src) {
+      srcArray.push({key: keys[i], imgSrc: src});
+    }
+  }
+
+  return srcArray;
+}
+
+function setImageSources(srcArray) {
+  if (srcArray.length == 0) return;
+
+  const mainImg = document.getElementById("projectinfo-img");
+  if (mainImg) {
+    mainImg.dataset.project = srcArray[0].key;
+    mainImg.src = srcArray[0].imgSrc;
+  }
+
+  if (srcArray.length == 1) return;
+  const otherImgs = document.getElementsByClassName("projectinfo-other");
+  for (let srci = 1, imgi = 0; srci < srcArray.length; srci++, imgi++) {
+    if (otherImgs[imgi] && srcArray[srci]) {
+      otherImgs[imgi].dataset.project = srcArray[srci].key;
+      otherImgs[imgi].src = srcArray[srci].imgSrc;
+    }
+  }
+}
+
+function setTitle(title) {
+  if (!title) return;
+
+  const el = document.getElementById("projectinfo-title");
+  if (el) {
+    el.innerText = title;
+  }
+}
+
+function setDescription(description) {
+  if (!description) return;
+
+  const el = document.getElementById("projectinfo-description");
+  if (el) {
+    el.innerText = description;
+  }
+}
+
+function setSubTitle(subtitle) {
+  if (!subtitle) return;
+
+  const el = document.getElementById("projectinfo-subtitle");
+  if (el) {
+    el.innerText = subtitle;
+  }
+}
+
+function setDetailsList(ls) {
+  if (!ls) return;
+
+  const ulElement = document.getElementById("projectinfo-detailslist");
+  if (!ulElement) return;
+
+  //if list and element are truthy, add truthy values from list to element as li elements
+  for (let i = 0; i < ls.length; i++) {
+    if (ls[i]) {
+      const li = document.createElement("li");
+      li.appendChild(document.createTextNode(ls[i]));
+      ulElement.appendChild(li);
+    }
+  }
+}
