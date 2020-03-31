0333c9a16aee460adf0f5c4cd8354cf11cc8a9cb
B.van.der.Wolf <benjamin.van.der.wolf@hva.nl>
2020-03-31 23:02:42 +0200

    Added Loading of nav bar html with javascript

---
+++ js/navBarConstructor.js
@@ -0,0 +1,25 @@
+const navBarHTMLStorageKey = "navigation";
+
+async function LoadNavBarASync(fromPage) {
+  if (!fromPage) return;
+
+  //get nav bar html from local storage
+  let text = localStorage.getItem(navBarHTMLStorageKey);
+  if (!text) {
+    //if nav bar html is not in local storage, fetch it and store it
+    text = await fetch("/pages/Navbar.html").then(Response => Response.text());
+    localStorage.setItem(navBarHTMLStorageKey, text);
+  }
+  //set inner html of navigation
+  const nav = document.getElementById("navigation");
+  nav.innerHTML = text;
+  //update active link based on given page
+  const links = nav.getElementsByClassName("nav-link");
+  for (let i = 0; i < links.length; i++) {
+    const el = links[i];
+    if (el.innerHTML == fromPage) {
+      el.classList.add("active");
+      break;
+    }
+  }
+}
