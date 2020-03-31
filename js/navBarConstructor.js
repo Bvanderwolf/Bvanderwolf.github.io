c90feb6b8e901df1c7346a762623df7a35691b12
B.van.der.Wolf <benjamin.van.der.wolf@hva.nl>
2020-04-01 00:20:21 +0200

    Updated navbar html with head and body

--- js/navBarConstructor.js
+++ js/navBarConstructor.js
@@ -7,10 +7,9 @@ async function LoadNavBarASync(fromPage) {
   let text = localStorage.getItem(navBarHTMLStorageKey);
   if (!text) {
     //if nav bar html is not in local storage, fetch it and store it
-    text = await fetch("/NavBar.html").then(Response => Response.text());
+    text = await fetch("/HtmlComponents/NavBar.html").then(Response => Response.text());
     localStorage.setItem(navBarHTMLStorageKey, text);
   }
-  console.log(text);
   //set inner html of navigation
   const nav = document.getElementById("navigation");
   nav.appendChild(new DOMParser().parseFromString(text, "text/html").body.firstElementChild);
